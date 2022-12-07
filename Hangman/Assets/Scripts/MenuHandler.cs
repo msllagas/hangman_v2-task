9a6fea636875487bf31e2eddf601b529a35b10b6
William <blazerx1234@gmail.com>
2022-12-07 15:37:35 +0800

    fuzzy update, updated w/ basis

--- Hangman/Assets/Scripts/MenuHandler.cs
+++ Hangman/Assets/Scripts/MenuHandler.cs
@@ -27,6 +27,8 @@ public class MenuHandler : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Debug.Log("menuhandler");
+
         UpdateStatsText();
         LoadBGMSession();
     }
